02899ca8263dc408515050d03fbaef4a88e66cd4
pranay <pranay.patil@shreshtait.com>
2024-10-04 12:07:10 +0530

    deleted website4

--- website4/dns-course/_next/static/m4zcINC4Ew2eveOG4RKGF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/733-04abdacfe4558683.js","static/chunks/pages/index-1af7540b379a439c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
